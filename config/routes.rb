aea6be3fecea3e10b2dd5e3c03a89b5233c0abd9
Fabrice Armisen <farmisen@g mail.com>
2015-11-20 17:25:42 -0800

    Adding devise settings for the normal users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users
+
   ActiveAdmin.routes(self)
   get 'dashboard/index'
 
